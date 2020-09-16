9e10072bd13b4436166fe1289a1cfc8341a1a7f9
James Pan <panjms@gmail.com>
2020-09-16 20:30:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
+//1
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
+
+//2
 function nowServing(katzDeliLine) {
+  //base case
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
+    //return current customer
     return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
+
+//3
 function currentLine(katzDeliLine) {
   let arr = [];
+  //array with line numbers embedded
   for (let i = 0; i < katzDeliLine.length; i++) {
     arr.push(`${i+1}. ${katzDeliLine[i]}`);
   }
-  
-  let str = "The line is currently: " + arr.join(', ');
-  
+
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    return str;
+    return "The line is currently: " + arr.join(', ');
   }
 }
